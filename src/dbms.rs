42e0b3c1e2a7d249ec4b69e401e95478f298a3f3
Michael Mileusnich <michaelmileusnich@Michaels-MacBook-Air.local>
2022-12-30 09:22:59 -0700

    Info files

--- src/dbms.rs
+++ src/dbms.rs
@@ -1,5 +1,10 @@
-use crate::dataspace::DataSpace;
 use std::collections::HashMap;
+use std::fs::File;
+use std::path::PathBuf;
+
+use log::error;
+
+use crate::dataspace::DataSpace;
 
 struct DBMS {
     data_directory: String,
@@ -14,6 +19,32 @@ impl DBMS {
         };
         dbms.load_dataspaces();
         dbms
+    }
+
+    fn info_file_path(&self) -> PathBuf {
+        let mut path = PathBuf::new();
+        path.push(self.data_directory.clone());
+        path.push("info");
+        path.set_extension("mike_db");
+        path
+    }
+
+    fn create_info_file(&self) -> File {
+        match File::create(self.info_file_path()) {
+            Ok(file) => file,
+            Err(e) => panic!("Could not create DBMS: {e}"),
+        }
+    }
+
+    fn load_info_file(&self) -> File {
+        match File::open(self.info_file_path()) {
+            Ok(file) => file,
+            Err(e) => match e.kind() {
+                std::io::ErrorKind::NotFound => self.create_info_file(),
+                std::io::ErrorKind::PermissionDenied => self.create_info_file(),
+                _ => panic!("Could not load DBMS: {e}"),
+            },
+        }
     }
 
     pub fn load_dataspaces(&mut self) {}
